ed5bfb341160de1fa6e1ce23e4b5d7d31e7dc198
Unknown <nico.fruta@gmail.com>
2018-03-18 13:17:56 -0300

    Playing with the API¶

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 
@@ -9,6 +11,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
